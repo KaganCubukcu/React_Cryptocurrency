d99cdb4da4c48418df9e8c65dd6749018d30d93c
Kağan Çubukçu <kagancancubukcu@gmail.com>
2022-08-06 10:37:07 +0300

    Edited api function

--- src/config/api.js
+++ src/config/api.js
@@ -4,7 +4,7 @@ export const CoinList = () =>
 export const SingleCoin = (id) =>
   `https://api.coingecko.com/api/v3/coins/${id}`;
 
-export const HistoricalChart = (id, days = 365, currency) =>
+export const HistoricalChart = (id, days = 365) =>
   `https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=TRY&days=${days}`;
 
 export const TrendingCoins = (currency) =>
